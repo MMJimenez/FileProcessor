ce771eac2b43e5a2c20fc114e745167cfb8cbd03
MiguelCSUR <93922638+MiguelCSUR@users.noreply.github.com>
2022-09-22 20:47:16 +0200

    first commit

---
+++ src/main/java/TextAnalizer.java
@@ -0,0 +1,9 @@
+import java.util.HashMap;
+
+public class TextAnalizer {
+    static HashMap<String, Integer> wordsFrequency = new HashMap<String, Integer>();
+
+    public static void wordCount(String text) {
+
+    }
+}
